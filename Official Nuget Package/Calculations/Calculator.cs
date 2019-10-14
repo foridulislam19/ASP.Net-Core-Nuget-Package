d978c1e057d531d9e2ecd0169c94326cdee6cfb0
shubhasish012 <shubhasish012@gmail.com>
2019-10-14 10:39:25 +0600

    Initialize

---
+++ Official Nuget Package/Calculations/Calculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Official_Nuget_Package.Calculations
+{
+    public class Calculator
+    {
+        public static double Add(double a,double b)
+        {
+            return a + b;
+        }
+    }
+}
